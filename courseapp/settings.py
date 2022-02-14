ae5e4026f185780d8ab303bcddcc46f1c3590653
erick <niteric@gmail.com>
2022-02-14 06:57:33 +0300

    Heroku commit5

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR, 'static']
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
