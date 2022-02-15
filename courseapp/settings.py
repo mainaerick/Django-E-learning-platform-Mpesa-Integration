dfc01a26f6d4c6486df824041feae437d4b030ca
erick <niteric@gmail.com>
2022-02-16 01:53:57 +0300

    Heroku commit12

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'courseapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'course/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
