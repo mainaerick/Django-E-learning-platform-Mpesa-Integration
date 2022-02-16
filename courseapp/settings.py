dc692a96e8b972cc75f138e27f3327cdd5bffe75
erick <niteric@gmail.com>
2022-02-16 23:44:12 +0300

    Heroku commit26

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [BASE_DIR, 'static']
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = BASE_DIR + '/static/media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
