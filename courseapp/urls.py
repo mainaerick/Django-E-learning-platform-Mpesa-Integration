c71bbcb4279e827105cc9ff6ca49ee94bf8ac4ee
erick <niteric@gmail.com>
2022-02-16 04:27:53 +0300

    Heroku commit23

--- courseapp/urls.py
+++ courseapp/urls.py
@@ -12,4 +12,4 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT[0])
     urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+                          document_root=settings.MEDIA_ROOT[0])
